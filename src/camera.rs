87ceeef9b61e7e5ba28cdf8290b18506c2efe41b
Emirhan Taşdeviren <emirhantasdeviren@gmail.com>
2024-07-30 17:07:31 +0300

    Define camera

---
+++ src/camera.rs
@@ -0,0 +1,11 @@
+use crate::vec::Vec3;
+
+pub struct Camera {
+    aspect_ratio: f32,
+    image_width: u32,
+    image_height: u32,
+    center: Vec3,
+    pixel00_loc: Vec3,
+    pixel_delta_u: Vec3,
+    pixel_delta_v: Vec3,
+}
